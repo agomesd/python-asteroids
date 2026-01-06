c264b6192e8d8bddc78a91d0ab72e7875b49b781
agomesd <alex.gduguay@gmail.com>
2026-01-06 16:51:04 +0000

    Player movement

--- main.py
+++ main.py
@@ -14,14 +14,20 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
